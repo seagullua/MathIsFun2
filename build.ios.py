1c6cbe8a47fac088acd62f770ee31d457306aa35
Andriy Chaika <andriy+en@chaika.in.ua>
2014-02-08 12:49:27 +0200

    iOS build

---
+++ build.ios.py
@@ -0,0 +1,14 @@
+from environment import *
+from libraries.ADLib.buildutil.ResourceOnlyProject import *
+
+working_directory = os.path.dirname(os.path.realpath(__file__))
+project = ResourceOnlyProject(env)
+project.resource_output = os.path.join(working_directory, 'proj.ios/Resources')
+project.resource_task = createResourceCompilerTask(
+    os.path.join(working_directory, 'Resources_storage'),
+    'mif',
+    'ios',
+    ['en','ru','es','ja','pt','de','fr','ko','tr','it','uk','nl','pl','hu']
+)
+
+project.buildProject()
