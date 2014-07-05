f6a9e7a0ad5e7014c5b8252d8f166b88d3f50733
Diana Sirenko <dianasirenko@gmail.com>
2014-07-05 18:00:59 +0300

    #24

--- proj.qt/main.cpp
+++ proj.qt/main.cpp
@@ -30,7 +30,7 @@ int CALLBACK WinMain(
 
 
     emulator->setFitTheScreen(true);
-    emulator->setDevice(Device::IPad2);
+    emulator->setDevice(Device::Galaxy_Tab2_10);
     emulator->setLanguage("en");
     emulator->setOrientation(Orientation::Landscape);
     return emulator->run();
