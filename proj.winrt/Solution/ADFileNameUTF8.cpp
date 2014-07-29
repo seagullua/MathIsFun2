314b48bc0dc79983619b55cb8cb77d243fb6296f
Andriy Chaika <andriy@chaika.in.ua>
2014-07-29 14:34:57 +0300

    Win8 Fix

--- proj.winrt/Solution/ADFileNameUTF8.cpp
+++
@@ -1,9 +0,0 @@
-#include "ADFileNameUTF8.h"
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
-#include "cocos2d.h"
-#include "CCWinRTUtils.h"
-const std::wstring ensureUtf8FileName(const std::string& f)
-{
-	return cocos2d::CCUtf8ToUnicode(f.c_str(), -1);
-}
-#endif
